2867718703c831bb3fc2b7a6aa9861a0e3b67a41
Sakti Dwi Cahyono <saktidc@icloud.com>
2022-05-26 22:22:51 +0700

    feat: Add fly.io config and add superuser setup

--- riemann/setup.py
+++ riemann/setup.py
@@ -10,6 +10,7 @@ django.setup()
 parser = argparse.ArgumentParser(description="setup script for django without using manage.py")
 parser.add_argument("--static", action="store_true", default=False)
 parser.add_argument("--migrate", action="store_true", default=False)
+parser.add_argument("--superuser", action="store_true", default=False)
 
 
 def main() -> None:
@@ -18,6 +19,8 @@ def main() -> None:
         call_command("collectstatic", interactive=False)
     if args.migrate:
         call_command("migrate", interactive=False)
+    if args.superuser:
+        call_command("createsuperuser", interactive=True)
 
 
 if __name__ == "__main__":
